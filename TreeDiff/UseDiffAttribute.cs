3bbbc49ef37d2f6db4a8d79174cfb83dd67216df
Igor <belovigor40000@gmail.com>
2023-03-01 13:21:41 +0300

    Cleanup

--- TreeDiff/UseDiffAttribute.cs
+++ TreeDiff/UseDiffAttribute.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TreeDiff
+﻿namespace TreeDiff
 {
 	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 	public class UseDiffAttribute : Attribute
